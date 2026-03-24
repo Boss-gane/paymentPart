471cddf516c806e1cae9d38033d93106731a146e
gbaggu-caregility <gbaggu@caregility.org>
2026-03-25 00:00:40 +0530

    www

---
+++ smartRepo/src/main/java/com/dmd/SmartRepoApplication.java
@@ -0,0 +1,13 @@
+package com.dmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartRepoApplication.class, args);
+	}
+
+}
---
+++ smartRepo/src/test/java/com/dmd/SmartRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
